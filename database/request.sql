a0b880177579114d113cff805c5b580e928ee968
zanzzxc <gxlee.2022@scis.smu.edu.sg>
2024-09-25 22:23:52 +0800

    request now has "type" field

--- database/request.sql
+++ database/request.sql
@@ -5,6 +5,7 @@ CREATE TABLE request (
     rid INT NOT NULL AUTO_INCREMENT,
     sid INT NOT NULL,
     status VARCHAR(50) DEFAULT 'Pending',
+    type varchar(50) not null,
     checkout_session_id VARCHAR(70),
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (rid),
